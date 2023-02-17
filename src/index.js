6566e4fe401ea54de3a56d7befb42dc4cfce3184
angelafegbai00 <120532203+angelafegbai00@users.noreply.github.com>
2023-02-17 00:27:19 +0000

    added my github link to the html and included the forecast

--- src/index.js
+++ src/index.js
@@ -26,6 +26,50 @@ function formDateValue() {
 }
 formDateValue();
 
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+        <div class=weather-forecast-date>${formatDay(
+          forecastDay.dt
+        )}</div>               
+        <img src="https://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="cloud" width="42" />
+         <div class="weather-forecast-temperature">
+           <span class="weather-forecast-temp-max"> ${Math.round(
+             forecastDay.temp.max
+           )}°
+             </span>
+                 <span class="weather-forecast-temp-min">
+                   ${Math.round(forecastDay.temp.min)}°
+                   </span>
+           </div>
+    </div>
+  `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "c544cc0b1b14fc3f9a412974ee9cff4a";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayForecast);
+}
+
 function searchCityButton(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#search-city-input");
@@ -35,9 +79,8 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchCityButton);
 
 function searchCity(city) {
-  let apiKey = "5d95fd50506eedab42e7a378d353b99a";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  let apiKey = "8628e1935308ab7a73f004d404aeot25";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
 
@@ -48,22 +91,21 @@ function showTemperature(response) {
   let cityElement = document.querySelector("#city");
   let iconElement = document.querySelector("#weatherIcon");
   let descriptionElement = document.querySelector("#description");
-  degreeTemperature = Math.round(response.data.main.temp);
+  degreeTemperature = Math.round(response.data.temperature.current);
 
-  cityElement.innerHTML = response.data.name;
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = degreeTemperature;
   document.querySelector(
     "#humidity"
-  ).innerHTML = `Humidity: ${response.data.main.humidity}`;
+  ).innerHTML = `Humidity: ${response.data.temperature.humidity}`;
   document.querySelector("#wind").innerHTML = `Wind: ${Math.round(
     response.data.wind.speed
   )}`;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  iconElement.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+  descriptionElement.innerHTML = response.data.condition.description;
+  iconElement.setAttribute("src", response.data.condition.icon_url);
+  iconElement.setAttribute("alt", response.data.condition.description);
+
+  getForecast(response.data.coordinates);
 }
 
 //Add a Current Location button. When clicking on it, it uses the Geolocation API to get your GPS coordinates and display and the city and current temperature using the OpenWeather API.
